cdffb4f16a7826df9de41404bdc2acdc899c2bf9
CoolStriker17 <3152074-CoolStriker17@users.noreply.replit.com>
2023-07-10 21:34:10 +0000

    new updates

--- main.py
+++ main.py
@@ -1,31 +1,116 @@
-import random
-#Arms - Biceps, Triceps, Shoulders
-#Legs - Hamstrings, Quadriceps, Calves, Glutes
-#Push - Chest, Shoulders, Triceps
-#Pull - Back and Biceps
-#Push - Chest and Back
-def getArms():
-  bicep_exercises = ["Seated Hammer Curl", "Bicep Curls", "EZ Bar Curl", "Preacher Curls", "Hammer Curl", "Chin Ups", "Dumbell Incline Bicep Curls"]
-  tricep_exercises = ["Dips", "Rope Pushdowns", "Skullcrushers", "Overhead Dumbell Extensions"]
-  shoulder_exercises = ["Seated Shoulder Press", "Arnold Press", "Front Press", "Lateral Raises", "Shoulder Shrugs", "Overhead Shoulder Press"]
-  random_b = random.randint(0, 5)
-  random_t = random.randint(0, 3)
-  random_s = random.randint(0, 5)
-  return(bicep_exercises[random_b] + ", "+ tricep_exercises[random_t] + ", " + shoulder_exercises[random_s]) 
+import discord
+import os
+from online import online
+from discord.ext import commands, tasks
+from discord import Embed, Color
 
 
-print("What type of split would you like to follow today:\n1. Arms (Biceps, Triceps, Shoulders)\n2.Legs (Hamstrings, Quadriceps, Calves, Glutes)\n3. Push Variation 1 (Chest, Shoulders, Triceps)\n4. Pull (Back and Biceps)\n5. Push Variation 2(Chest and Back)")
-#1 - Arms, 2 - Legs, 3 - Push 1, 4 - Pull, 5 - Push 2
-split = input()
-print("Would you like to focus on a specific muscle in this group?")
-response = input("Y/N: ")
-if response == "Y":
-  specific = True
-  print("What kind of muscle would you like to focus on?")
-  response2 = input()
-else:
-  specific = False
+#bicep_exercises = ["Seated Hammer Curl", "Bicep Curls", "EZ Bar Curl", "Preacher Curls", "Hammer Curl", "Chin Ups", "Dumbell Incline Bicep Curls"]
+#  tricep_exercises = ["Dips", "Rope Pushdowns", "Skullcrushers", "Overhead Dumbell Extensions"]
+#  shoulder_exercises = ["Seated Shoulder Press", "Arnold Press", "Front Press", "Lateral Raises", "Shoulder Shrugs", "Overhead Shoulder Press"]
 
-if int(split) == 1:
-  print(getArms())
+intents = discord.Intents.all()
+intents.members = True
+intents.reactions = True
+client = commands.Bot(command_prefix="-", intents=intents)
 
+@client.event
+async def on_ready():
+    print("KACHOW")
+
+
+def buttons(message, l, r):
+    def check(reaction, user):
+        if reaction.message.id != message.id or user == client.user:
+            return False
+        if l and reaction.emoji == "⏪":
+            return True
+        if r and reaction.emoji == "⏩":
+            return True
+        return False
+
+    return check
+
+@client.command()
+async def gym(ctx):
+    guild = ctx.guild
+    await ctx.message.delete()
+    embed = discord.Embed(
+        title = 'Gym',
+        description = 'Arms - 🏋️\t\n Push - 💪\t\n Pull - 🎉\t\n Chest&Back - ⚙️\t\n Legs - 🦵',
+        color = 0xb042f5
+    )
+    embed.set_footer(text=f"Gym")
+    msg = await ctx.send(embed=embed)
+  
+    await msg.add_reaction("🏋️")
+    await msg.add_reaction("💪")
+    await msg.add_reaction("🎉")
+    await msg.add_reaction("⚙️")
+    await msg.add_reaction("🦵")
+
+    @client.event
+    async def on_reaction_add(reaction, user):
+      if user == "Gym_Bot#6486":
+        pass
+      else:
+        if reaction.emoji == "🏋️" and user != "Gym_Bot#6486":
+          pages = []
+          await msg.delete()
+          embedVar = discord.Embed(title=f"Gym",
+                                   colour=0xb042f5)
+          embedVar.add_field(
+              name="Biceps",
+              value=
+              "1. Seated Hammer Curls\n2. Bicep Curls\n3. EZ Bar Curls\n 4. Preacher Curls\n 5. Hammer Curl \n 6. Chin Ups \n 7. Dumbell Incline Bicep Curls"
+          )
+          embedVar2 = discord.Embed(title=f"Gym",
+                                   colour=0xb042f5)
+          embedVar2.add_field(
+              name="Triceps",
+              value=
+              "1. Dips\n2. Rope Pushdowns\n3. Skullcrushers\n4. Overhead Dumbell Extensions"
+          )
+          embedVar3 = discord.Embed(title=f"Gym",
+                                   colour=0xb042f5)
+          embedVar3.add_field(
+              name="Shoulders",
+              value=
+              "1. Seated Shoulder Press\n2. Arnold Press\n3. Front Press\n4. Lateral Raises\n5. Shoulder Shrugs\n6. Overhead Shoulder Press"
+          )
+          pages.append(embedVar)
+          pages.append(embedVar2)
+          pages.append(embedVar3)
+          page = 0
+          left = "⏪"
+          right = "⏩"
+          while True:
+              msg2 = await ctx.send(embed=pages[(page)])
+              l_reaction = page != 0
+              r_reaction = page <= len(pages) - 1
+              if l_reaction:
+                  await msg2.add_reaction(left)
+              if r_reaction:
+                  if page == (len(pages) - 1):
+                      react = await client.wait_for('reaction_add',
+                                                    check=buttons(
+                                                        msg2, l_reaction, r_reaction))
+                  else:
+                      await msg2.add_reaction(right)
+                      react = await client.wait_for('reaction_add',
+                                                    check=buttons(
+                                                        msg2, l_reaction, r_reaction))
+      
+              if str(react[0]) == left:
+                  page -= 1
+              elif str(react[0]) == right:
+                  page += 1
+      
+              await msg2.delete()
+      
+
+
+
+online()
+token = os.environ.get("TOKEN")
+client.run(token)
